8b8e1c51c0639043c4334e516171b6f8eddc1de2
GeorgeVelev <georgi.velev@scalefocus.com>
2018-07-11 10:38:08 +0300

    Payment model added. Needs further work.

---
+++ src/main/java/project/model/Payment.java
@@ -0,0 +1,31 @@
+package project.model;
+
+public class Payment {
+
+    private String cardIBAN;
+    private String cash;
+
+    public Payment() {
+    }
+
+    public Payment(String cardIBAN, String cash) {
+        this.cardIBAN = cardIBAN;
+        this.cash = cash;
+    }
+
+    public String getCardIBAN() {
+        return cardIBAN;
+    }
+
+    public void setCardIBAN(String cardIBAN) {
+        this.cardIBAN = cardIBAN;
+    }
+
+    public String getCash() {
+        return cash;
+    }
+
+    public void setCash(String cash) {
+        this.cash = cash;
+    }
+}
